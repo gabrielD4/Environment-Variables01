64c63635ba7b48b2cad565999afd19011b466570
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-06-06 21:26:44 +0200

    fix

--- src/main/java/com/example/ex20environment_variables01/Controller.java
+++ src/main/java/com/example/ex20environment_variables01/Controller.java
@@ -1,7 +1,6 @@
 package com.example.ex20environment_variables01;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,7 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping("properties")
-public class controller {
+public class Controller {
 
     @Autowired
     private PropertiesService propertiesService;
